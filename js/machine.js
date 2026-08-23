455cf552ce143a75add77a53a90201732982ea46
Shua Deitsch <shuadeitsch@gmail.com>
2026-08-23 19:40:20 +0000

    Force the machine tab to load the new page instead of staying on the first job.

--- js/machine.js
+++ js/machine.js
@@ -3,7 +3,16 @@ function startMachineJob(start, end) {
   var e = parseInt(end, 10);
   if (!Number.isFinite(s)) return;
   if (!Number.isFinite(e) || e < s) e = s;
-  var url = "http://127.0.0.1:8765/retype-movie.html?start=" + s + "&end=" + e + "&write=1";
+  var url = "http://127.0.0.1:8765/retype-movie.html?start=" + s + "&end=" + e + "&write=1&t=" + Date.now();
   var w = window.open(url, "sichos-machine");
-  if (!w) window.location.href = url; // last resort
+  if (!w) {
+    window.location.href = url;
+    return;
+  }
+  try {
+    w.location.href = url;
+  } catch (err) {}
+  try {
+    w.focus();
+  } catch (err) {}
 }
